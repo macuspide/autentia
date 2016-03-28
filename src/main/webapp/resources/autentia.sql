f15d2a250af336e8500a09308238f7805c624b5c
Macuspide <macuspide@gmail.com>
2016-03-28 19:12:40 +0200

    subo el sql

--- src/main/webapp/resources/autentia.sql
+++ src/main/webapp/resources/autentia.sql
@@ -1,14 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-03-2016 a las 18:08:10
+-- Versión del servidor: 10.1.9-MariaDB
+-- Versión de PHP: 5.6.15
 
-CREATE TABLE IF NOT EXISTS `cursos` (
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `autentia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cursos`
+--
+
+CREATE TABLE `cursos` (
   `id_curso` int(11) NOT NULL,
   `titulo` varchar(30) NOT NULL,
-  `id_nivel` int(11) NOT NULL,
+  `id_nivel` int(11) DEFAULT NULL,
   `horas` int(11) NOT NULL,
   `activo` tinyint(1) NOT NULL,
-  `id_profesor` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `id_profesor` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
+-- Volcado de datos para la tabla `cursos`
+--
 
 INSERT INTO `cursos` (`id_curso`, `titulo`, `id_nivel`, `horas`, `activo`, `id_profesor`) VALUES
 (1, 'Introducción a JSF2', 2, 25, 1, 1),
@@ -16,60 +45,105 @@ INSERT INTO `cursos` (`id_curso`, `titulo`, `id_nivel`, `horas`, `activo`, `id_p
 (3, 'Novedades Java 8', 2, 16, 1, 1),
 (4, 'Angular 2', 3, 20, 0, 2),
 (5, 'NoSQL', 3, 10, 0, 2),
-(6, 'Android avanzado', 3, 25, 1, 1);
+(6, 'Android avanzado', 3, 25, 1, 1),
+(20, 'Javascript', 2, 22, 1, 2),
+(21, 'PHP', 1, 43, 1, 1),
+(23, 'Fotografia I', 3, 46, 0, 3);
 
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `nivel`
+--
 
-CREATE TABLE IF NOT EXISTS `nivel` (
+CREATE TABLE `nivel` (
   `id_nivel` int(11) NOT NULL,
   `nombre_nivel` varchar(30) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
+-- Volcado de datos para la tabla `nivel`
+--
 
 INSERT INTO `nivel` (`id_nivel`, `nombre_nivel`) VALUES
-(1, 'Basico'),
+(1, 'Básico'),
 (2, 'Intermedio'),
 (3, 'Avanzado');
 
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `profesores`
+--
 
-CREATE TABLE IF NOT EXISTS `profesores` (
+CREATE TABLE `profesores` (
   `id_profesor` int(11) NOT NULL,
   `nombre_profe` varchar(30) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
+-- Volcado de datos para la tabla `profesores`
+--
 
 INSERT INTO `profesores` (`id_profesor`, `nombre_profe`) VALUES
 (1, 'Francisco Garrido Gil'),
-(2, 'Paquita Rosales Cruz');
+(2, 'Paquita Rosales Cruz'),
+(3, 'Jose Pepe');
 
+--
+-- Índices para tablas volcadas
+--
 
+--
+-- Indices de la tabla `cursos`
+--
 ALTER TABLE `cursos`
   ADD PRIMARY KEY (`id_curso`),
   ADD KEY `id_profesor` (`id_profesor`),
   ADD KEY `id_nivel` (`id_nivel`);
 
-
+--
+-- Indices de la tabla `nivel`
+--
 ALTER TABLE `nivel`
   ADD PRIMARY KEY (`id_nivel`);
 
-
+--
+-- Indices de la tabla `profesores`
+--
 ALTER TABLE `profesores`
   ADD PRIMARY KEY (`id_profesor`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
+--
+-- AUTO_INCREMENT de la tabla `cursos`
+--
 ALTER TABLE `cursos`
-  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
-
+  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+--
+-- AUTO_INCREMENT de la tabla `nivel`
+--
 ALTER TABLE `nivel`
-  MODIFY `id_nivel` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
-
+  MODIFY `id_nivel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `profesores`
+--
 ALTER TABLE `profesores`
-  MODIFY `id_profesor` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id_profesor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Restricciones para tablas volcadas
+--
 
+--
+-- Filtros para la tabla `cursos`
+--
 ALTER TABLE `cursos`
   ADD CONSTRAINT `fk_curso_nivel` FOREIGN KEY (`id_nivel`) REFERENCES `nivel` (`id_nivel`),
   ADD CONSTRAINT `fk_curso_profe` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`);
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
