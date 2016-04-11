62b9bf2c2ca3c0964eb5b7d6d6d01e586cf1fbdc
Macu Gómez <igomez@MacBook-Pro-igomez.local>
2016-04-11 17:05:44 +0200

    subiendo test

--- src/test/java/com/prueba/services/NivelServiceIT.java
+++ src/test/java/com/prueba/services/NivelServiceIT.java
@@ -1,11 +1,19 @@
 package com.prueba.services;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.prueba.beans.NivelBean;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:root-context-test.xml"})
@@ -14,9 +22,36 @@ public class NivelServiceIT {
 	@Autowired
 	NivelService sut;
 	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
 	@Test
 	public void getNivelByIdShouldReturnNotNull(){
 		sut.getNivel(1);
 	}
+	
+	@Test
+	public void getNivelByIdShouldReturnNNull(){
+		sut.getNivel(1);
+	}
+	
+	@Test
+	public void getNivelByIdShouldReturnNivelBean(){
+		assertEquals(sut.getNivel(1).getClass(), NivelBean.class);
+	}
+	
+	@Test
+	public void getAllNivelesShouldReturnNotNull(){
+		sut.getAllNiveles();
+	}
+	
+	@Test
+	public void getAllNivelesShouldReturnList(){
+		assertEquals(sut.getAllNiveles().getClass(), ArrayList.class);
+		sut.getAllNiveles();
+	}
+	
+	//expectedException.expect(RuntimeException.class);
+	//expectedException.expectMessage("Error al ejecutar la consulta");
 	
 }
